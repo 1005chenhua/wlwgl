9ac0d772b4d63142de98cb672d53aded36876967
jyhhyj <1257259402@qq.com>
2019-11-14 19:20:56 +0800

    fix: 信息采集优化

--- src/pages/deviceManage/infoAnasy/index.js
+++ src/pages/deviceManage/infoAnasy/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { Progress,Button,Pagination } from 'antd';
+import { Progress,Button,Pagination,Select } from 'antd';
 import { RouteList, asyncRoutes } from '@/router';
 import { createHashHistory } from 'history';
 import Header from '@/components/header';
 import _ from 'lodash';
+
+const { Option } = Select;
 const history = createHashHistory();
 
 const inforAnasyData = [
@@ -23,6 +25,9 @@ const inforAnasyData = [
 // eslint-disable-next-line complexity
 export default ({ match }) => {
   const [visible, setVisible] = useState('0');
+  const handleChange = (value) => {
+    console.log(value);
+  };
   return (
     <div className="layout-container">
       <Header />
@@ -31,7 +36,43 @@ export default ({ match }) => {
           <p>位置信息 : <span>设备管理/信息采集设备</span></p>
         </div>
         <div className="inforAnasy-container">
-          <div className="inforAnasy-container-top">11</div>
+          <div className="inforAnasy-container-top">
+            <div className="inforAnasy-container-top-btn">
+              <Button type="link">添加设备</Button>
+              <Button type="link">批量导入</Button>
+              <Button type="link">下载模板</Button>
+              <Button type="link">导出EXCEL</Button>
+              <Button type="link">导出二维码</Button>
+              <Button type="link">切换</Button>
+            </div>
+            <div className="inforAnasy-container-top-input">
+              <div className="dev-search-select">
+                <span className="dev-search-select-title">资产编号</span>
+                <input type="text" />
+              </div>
+              <div className="dev-search-select">
+                <span className="dev-search-select-title">设备名称</span>
+                <input type="text" />
+              </div>
+              <div className="dev-search-select">
+                <span className="dev-search-select-title">资产种类</span>
+                <Select defaultValue="lucy" style={{ width: 200 }} onChange={handleChange}>
+                  <Option value="jack">Jack</Option>
+                  <Option value="lucy">Lucy</Option>
+                  <Option value="Yiminghe">yiminghe</Option>
+                </Select>
+              </div>
+              <div className="dev-search-select">
+                <span className="dev-search-select-title">资产种类</span>
+                <Select defaultValue="lucy" style={{ width: 200 }} onChange={handleChange}>
+                  <Option value="jack">Jack</Option>
+                  <Option value="lucy">Lucy</Option>
+                  <Option value="Yiminghe">yiminghe</Option>
+                </Select>
+              </div>
+              <Button type="link">搜索</Button>
+            </div>
+          </div>
           <div className="inforAnasy-container-bottom">
             {
               _.map(inforAnasyData, item => (<div className="inforAnasy-box">
