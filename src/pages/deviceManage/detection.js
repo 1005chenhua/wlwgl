2f1311066f0b377c562d71c05d808e9228cbf3db
jyhhyj <1257259402@qq.com>
2019-11-14 18:42:36 +0800

    feat: 信息采集功能添加

--- src/pages/deviceManage/detection.js
+++ src/pages/deviceManage/detection.js
@@ -1,3 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createHashHistory } from 'history';
+import _ from 'lodash';
+const history = createHashHistory();
 
-export default () => (<div>检测分析111111111111111111</div>);
+const totalData = [
+  { number: '123456',text: '设备总数' },
+  { number: '89%',text: '设备完好率' },
+  { number: '254',text: '告警中心' },
+];
+const detection = () => {
+  return (
+    <div className="detection-container">
+      <div className="detection-container-left">
+          1
+      </div>
+      <div className="detection-container-right">
+        <div>1</div>
+        <div>1</div>
+      </div>
+    </div>
+  );
+};
+
+
+export default connect()(detection);
