297fc68c854d933a356f96cbbb30785d0d27b853
jyhhyj <1257259402@qq.com>
2019-11-14 14:43:07 +0800

    style: 设备管理

--- src/pages/deviceManage/index.js
+++ src/pages/deviceManage/index.js
@@ -1,31 +1,17 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
-import { RouteList, asyncRoutes } from '@/router';
+import { Menu } from 'antd';
+import { RouteList } from '@/router';
 import { createHashHistory } from 'history';
 import Header from '@/components/header';
+import Devmanage from './devManage';
 
-const { SubMenu } = Menu;
 const history = createHashHistory();
 
-const recursion = (dataSource, match) => {
-  return (
-    dataSource.map(menu => {
-      if (menu.children) {
-        return (
-          <SubMenu key={menu.id} title={menu.title}>
-            {recursion(menu.children, match)}
-          </SubMenu>
-        );
-      }
-      return (<Menu.Item key={menu.id} onClick={e => history.push(`${match.url + menu.path}`)}>{menu.title}</Menu.Item>);
-    })
-  );
-};
-export default ({ match }) => (
+export default () => (
   <div className="layout-container">
     <Header />
-    <div>
-      <RouteList match={ match } />
+    <div className="layout-container-content" >
+      <Devmanage />
     </div>
   </div>
 );
