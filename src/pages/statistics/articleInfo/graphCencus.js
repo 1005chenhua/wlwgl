d4cfeef264ed2438ea0124a428911453b52492f7
jyhhyj <1257259402@qq.com>
2019-11-21 17:11:22 +0800

    fix: 模块样式搭建

---
+++ src/pages/statistics/articleInfo/graphCencus.js
@@ -0,0 +1,14 @@
+/* eslint-disable semi */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react';
+import { createHashHistory } from 'history';
+
+const history = createHashHistory();
+// eslint-disable-next-line complexity
+export default ({ match }) => {
+  return (
+    <div className="inforStatic-container-right">
+      运动康复
+    </div>
+  );
+};
--- src/pages/statistics/inforStatic.js
+++ src/pages/statistics/inforStatic.js
@@ -1,27 +1,50 @@
+/* eslint-disable default-case */
+/* eslint-disable no-magic-numbers */
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { createHashHistory } from 'history';
 import { Select,Button,DatePicker } from 'antd';
-import ArticeCencus from './vitalSigns/graphCencus';
+import ArticeCencus from './vitalSigns/graphCencus'; // 生命体征
+import ArticleInfo from './articleInfo/graphCencus'; // 物品信息
 
 import _ from 'lodash';
 const { Option } = Select;
 const inforStatic = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [visible, setVisible] = useState(0);
+  const contentList = [
+    { name: '生命体征',id: 0 },
+    { name: '运动康复',id: 1 },
+    { name: '物品信息',id: 2 },
+    { name: '戒毒医疗',id: 3 },
+    { name: '环境信息',id: 4 },
+    { name: '能耗检测',id: 5 },
+  ];
   return (
     <div className="inforStatic-container">
       <div className="inforStatic-container-left">
         <div className="inforStatic-container-left-bg" />
         <div className="inforStatic-container-left-textBox" >
-          <div className="content">生命体征</div>
-          <div className="content">运动康复</div>
-          <div className="content">物品信息</div>
-          <div className="content">戒毒医疗</div>
-          <div className="content">环境信息</div>
-          <div className="content">能耗检测</div>
+          {
+            _.map(contentList, item => (
+              <div className="content" key={item.id} onClick={() => {
+                setVisible(item.id);
+              }}>{item.name}</div>
+            ))
+          }
         </div>
       </div>
       <div className="inforStatic-container-right">
-        <ArticeCencus />
+        { (() => {
+          switch (visible) {
+            case 0:
+              return <ArticeCencus />;
+            case 1:
+              return <ArticleInfo />;
+          }
+        })()
+        }
+        {/* <ArticeCencus /> */}
       </div>
     </div>
   );
